bd16fd6536a5dcbe1bae7d228ed35f70e6a8d4c3
koval.andriy.cr <koval.andriy.cr@gmail.com>
2018-11-27 17:30:04 +0200

    - s3 sns subscription fix

--- server/helpers/gpsHelper.js
+++ server/helpers/gpsHelper.js
@@ -1,10 +1,20 @@
 
 class GpsHelper {
     static formatData(gpsExif) {
-        return {
-            lat: `${gpsExif.GPSLatitude[0]}.${gpsExif.GPSLatitude[1]}${Math.round(gpsExif.GPSLatitude[2])}`,
-            lng: `${gpsExif.GPSLongitude[0]}.${gpsExif.GPSLongitude[1]}${Math.round(gpsExif.GPSLongitude[2])}`
+        const coords = {};
+
+        coords.lat = `${gpsExif.GPSLatitude[0]}.${gpsExif.GPSLatitude[1]}${Math.round(gpsExif.GPSLatitude[2])}`;
+        coords.lng = `${gpsExif.GPSLongitude[0]}.${gpsExif.GPSLongitude[1]}${Math.round(gpsExif.GPSLongitude[2])}`;
+
+        if(gpsExif.GPSLatitudeRef === 'S') {
+            coords.lat = -coords.lat
         }
+
+        if(gpsExif.GPSLongitudeRef === 'W') {
+            coords.lng = -coords.lng
+        }
+
+        return coords
     }
 }
 
